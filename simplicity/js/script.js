81411419ba658f0c4641102a77e309be0deab36d
leonardo <leorsousa05@gmail.com>
2022-04-19 15:07:46 -0300

    Programação do CEP e Endereço
    Uso de ajax com a função fetch e da lib VMasker para máscaras de telefone e CEP.

--- simplicity/js/script.js
+++ simplicity/js/script.js
@@ -1,12 +1,52 @@
-const menu = document.querySelector('#menu');
-const nav = document.querySelector('.links-menu');
+const formulario = document.querySelector("form");
+const inputCep = formulario.querySelector("#cep");
+const inputTelefone = formulario.querySelector("#telefone");
+const inputEndereco = formulario.querySelector("#endereco");
+const inputBairro = formulario.querySelector("#bairro");
+const inputCidade = formulario.querySelector("#cidade");
+const inputEstado = formulario.querySelector("#estado");
+const bStatus = formulario.querySelector("#status");
+const botaoLocalizar = formulario.querySelector("#localizar-cep");
 
-menu.addEventListener('click', function(event) {
-    event.preventDefault()
-    nav.classList.toggle('aberto');
-    if( nav.classList.contains("aberto")) {
-        menu.innerHTML = "Fechar &#x2715";
-    } else {
-        menu.innerHTML = "Menu &equiv;";
-    }
+botaoLocalizar.addEventListener('click', function(event) {
+    event.preventDefault();
+    // Entre no site: viacep.com.br
+
+    /* Pegar o cep digitado */
+    let cep = inputCep.value;
+
+    /* CEP no padrão da API */
+    let url = `https://viacep.com.br/ws/${cep}/json/`;
+    
+    /* Ajax: Comunicação Assíncrona com
+    o ViaCep usando a função chamada fetch. */
+
+    // 1) Fazer conexão com API (ou acessar)
+    fetch(url)
+
+        // 2) E então, Recupere a resposta do acesso no formato JSON
+        .then(resposta => resposta.json())
+
+        // 3) E então, mostre os dados
+        .then( dados => {
+            if ('erro' in dados) {
+                bStatus.innerHTML = "CEP não encontrado!"
+                inputCep.focus()
+            } else {
+                bStatus.innerHTML = "CEP encontrado!";
+                inputEndereco.value = dados.logradouro
+                inputBairro.value = dados.bairro;
+                inputCidade.value = dados.localidade
+                inputEstado.value = dados.uf
+            }
+            console.log(dados);
+            inputEndereco.value = dados.logradouro;
+        });
 });
+
+/* Lib VanillaMask: 
+https://github.com/vanilla-masker/vanilla-masker*/
+
+VMasker(inputCep).maskPattern("99999-999");
+VMasker(inputTelefone).maskPattern("(99) 99999-9999");
+
