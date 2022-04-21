4e2aa9476a1c3a79979c01c4acecca8a56a83b6c
leorsousa05 <leorsousa05@gmail.com>
2022-04-21 20:18:37 -0300

    Chuva challenge new Changes

---
+++ chuva-challenge/js/script.js
@@ -0,0 +1,9 @@
+let btnContainer = document.getElementById("nav-menu");
+
+
+let allbtns = document.getElementsByClassName('menu-item')
+let btns = allbtns
+
+btns.addEventListener("click", function() {
+    console.log("reoi");
+})
